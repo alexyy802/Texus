30495fb1dda1d3a7d6e2687ecdbab11a7e6c3e73
Vincent <vincentbusiness55@gmail.com>
2021-11-18 03:17:22 -0800

    Fixing Uploading

--- discord/__init__.py
+++ discord/__init__.py
@@ -14,7 +14,7 @@ __title__ = 'texus'
 __author__ = 'TexusDev'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015-2021 Rapptz, Copyright 2021-present TexusDev'
-__version__ = '2.0.0a'
+__version__ = '2.0.0'
 
 __path__ = __import__('pkgutil').extend_path(__path__, __name__)
 
@@ -70,6 +70,6 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: VersionInfo = VersionInfo(major=2, minor=0, micro=0, releaselevel='alpha', serial=0)
+version_info: VersionInfo = VersionInfo(major=2, minor=0, micro=0, releaselevel='final', serial=0)
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
